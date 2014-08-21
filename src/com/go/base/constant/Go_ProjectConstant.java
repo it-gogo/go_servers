9b112582129edf297e20ff50dccfcaae3dd04da4
it-gogo <lyb269@qq.com>
2014-08-21 09:54:57 +0800

    2014-8-21  linyb  项目常量类更改

--- src/com/go/base/constant/Go_ProjectConstant.java
+++ src/com/go/base/constant/Go_ProjectConstant.java
@@ -8,27 +8,7 @@ package com.go.base.constant;
 public class Go_ProjectConstant {
 	
 	/**
-	 * 基本账户充值
-	 */
-	public static String HZ_BASIC_ACCOUNT_RECHARGE = "HZ_BASIC_ACCOUNT_RECHARGE";
-	
-	/**
-	 * 产品办理记录
-	 */
-	public static String HZ_FINANCIAL_CREDIT_LOAN_RECORD = "HZ_FINANCIAL_CREDIT_LOAN_RECORD";
-	
-	/**
-	 * 默认账户密码
-	 */
-	public static String HZ_ACCOUNT_DEFAULT_PASSWORD = "365365";	
-	
-	/**
-	 * 用户单点登录全局变量
-	 */
-	public static String HZ_SINGLETON_LOGIN_MAP = "sloginMap";
-	
-	/**
 	 * 文件上传路径
 	 */
-	public static String HZ_UPLOAD_FILES = "upload_files";
+	public static String GO_UPLOAD_FILES = "upload_files";
 }
