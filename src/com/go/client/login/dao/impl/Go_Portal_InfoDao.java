e1f1ad41476e2de80bf1f2ee566961bc77efda15
chenhb <641484739@qq.com>
2014-08-31 17:26:06 +0800

    Signed-off-by: chenhb <641484739@qq.com>

--- src/com/go/client/login/dao/impl/Go_Portal_InfoDao.java
+++ src/com/go/client/login/dao/impl/Go_Portal_InfoDao.java
@@ -6,9 +6,11 @@ import org.springframework.stereotype.Repository;
 
 import com.go.base.dao.impl.Go_BaseDao;
 import com.go.client.login.dao.IGo_Portal_InfoDao;
+import com.go.client.login.mapper.IGo_Portal_InfoMapper;
 import com.go.client.login.model.Go_Portal_Info;
 import com.go.client.main.dao.IGo_Login_InfoDao;
 import com.go.sys.authority.model.Go_Department_Info;
+import com.go.sys.common.mapper.IGo_Common_DataMapper;
 
 /**
  * 门户信息 数据层实现类
@@ -19,6 +21,7 @@ public class Go_Portal_InfoDao extends Go_BaseDao<Go_Portal_Info, Integer> imple
 	@Autowired
 	public Go_Portal_InfoDao(SqlSessionFactory sqlSessionFactory) {
 		super(sqlSessionFactory);
+		this.setMapperClass(IGo_Portal_InfoMapper.class);
 		// TODO Auto-generated constructor stub
 	}
 }
